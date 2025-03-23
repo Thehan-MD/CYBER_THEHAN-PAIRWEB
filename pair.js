8e32aa02c24049d67f4501faf0ef476da56babe4
Thehan-MD <veronica1978online@gmail.com>
2025-03-23 14:12:21 +0530

    Delete pair.js

--- pair.js
+++
@@ -1,4 +0,0 @@
-function generatePairCode() {
-    const code = 'PAIR-' + Math.random().toString(36).substring(2, 10).toUpperCase();
-    document.getElementById("pairCode").innerText = `Your Pair Code: ${code}`;
-}
