dacabec4cbdf780a6f3d08122700da9fa59169e0
Thehan-MD <veronica1978online@gmail.com>
2025-03-23 14:12:42 +0530

    Delete thehanqr.js

--- thehanqr.js
+++
@@ -1,4 +0,0 @@
-function generateQRCode() {
-    const qrCode = 'QR-' + Math.random().toString(36).substring(2, 10).toUpperCase();
-    document.getElementById("qrcode").innerHTML = `Your QR Code: ${qrCode}`;
-}
